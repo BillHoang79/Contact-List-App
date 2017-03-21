dbdf62e1a4f2f4197cbff32f4543be2d807c5fef
billhoang79 <billhoang79@gmail.com>
2017-03-21 15:43:56 -0700

    adding data, sending and recieving data

--- server.js
+++ server.js
@@ -3,5 +3,30 @@ var app     = express()
 
 app.use(express.static(__dirname + "/public"))
 
+app.get('/contactlist', function (req, res) {
+	console.log("I received a GET request")
+
+	person1 = {
+			name: 'Rick',
+			email: 'rick@gmail.com',
+			number: '(222) 222-2222'
+		},
+
+		person2 = {
+			name: 'John',
+			email: 'john@gmail.com',
+			number: '(333) 333-3333'
+		},
+
+		person3 = {
+			name: 'Hilda',
+			email: 'hilda@gmail.com',
+			number: '(444) 444-4444'
+		}
+
+		var contactlist = [person1, person2, person3]
+		res.json(contactlist)
+})
+
 app.listen(3000)
 console.log('server running on port 3000')
