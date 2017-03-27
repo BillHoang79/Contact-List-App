20f5d20e1e74e5e802832913741b320c6a51e8f4
Bill Hoang <billhoang79@gmail.com>
2017-03-26 18:15:48 -0700

    made some changes

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,9 +1,8 @@
 var myApp = angular.module('myApp',[])
-myApp.controller('AppCtrl',['$scope','$http',
-	function($scope, $http) {
+myApp.controller('AppCtrl', ['$scope','$http', function($scope, $http) {
 		console.log("Hello World from controller!!!")
 
-var refesh = function() {
+var refresh = function() {
 	$http.get('/contactlist').success(function(response) {
 		console.log("I got the data I requested")
 		$scope.contactlist = response
@@ -11,13 +10,13 @@ var refesh = function() {
 	})
 }
 
-refesh()
+refresh()
    
 $scope.addContact = function() {
 	console.log($scope.contact)
 	$http.post('/contactlist', $scope.contact).success(function(response) {
 		console.log(response)
-		refesh()
+		refresh()
 	})
 
 }
@@ -25,22 +24,22 @@ $scope.addContact = function() {
 $scope.remove = function(id) {
 	console.log(id)
 	$http.delete('/contactlist/' + id).success(function(response) {
-		refesh()
+		refresh()
 	})
 }
 
 $scope.edit = function(id) {
-	console.log(id)
-	$http.get('/contactlist/' + id ).success(function(response) {
-		$scope.contact = response
-	})
-}
+  console.log(id);
+  $http.get('/contactlist/' + id).success(function(response) {
+    $scope.contact = response;
+  })
+} 
 
 $scope.update = function() {
-	console.log($scope.contact._id)
-	$http.put('/contactlist/' + $scope.contact._id, $scope.contact).success(function(response) {
-		refresh()
-	})
+  console.log($scope.contact._id)
+  $http.put('/contactlist/' + $scope.contact._id, $scope.contact).success(function(response) {
+    refresh()
+  })
 }
 
 $scope.deselect = function() {
