8e0134769b3108dcc12bff22587edebeeda1cc92
Bill Hoang <billhoang79@gmail.com>
2017-04-03 18:27:40 -0700

    redefined angular

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var mongojs = require('mongojs')
 var db = mongojs('contactlist', ['contactlist'])
 var bodyParser = require('body-parser')
 var port = Number(process.env.PORT || 8000)
+var angular = require('angular')
 
 
 
