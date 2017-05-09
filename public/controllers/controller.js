0f599cf5800e44653b6be17dca8f010a9c644a89
Bill Hoang <billhoang79@gmail.com>
2017-05-09 00:21:37 -0700

    added some code

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,49 +1,49 @@
-var myApp = angular.module('myApp', [])
+var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
-    console.log("Hello World from controller!!!")
+    console.log("Hello World from controller!!!");
 
     var refresh = function() {
         $http.get('/contactlist').success(function(response) {
-            console.log("I got the data I requested")
-            $scope.contactlist = response
-            $scope.contact = ""
-        })
-    }
+            console.log("I got the data I requested");
+            $scope.contactlist = response;
+            $scope.contact = "";
+        });
+    };
                
-    refresh()
+    refresh();
 
     $scope.addContact = function() {
-        console.log($scope.contact)
+        console.log($scope.contact);
         if ($scope.contact._id !== null) {
             $scope.contact._id = null;
-        }
+        };
         $http.post('/contactlist', $scope.contact).success(function(response) {
-            console.log(response)
-            refresh()
-        })
+            console.log(response);
+            refresh();
+        });
 
-    }
+    };
 
     $scope.remove = function(id) {
-        console.log(id)
+        console.log(id);
         $http.delete('/contactlist/' + id).success(function(response) {
-            refresh()
-        })
-    }
+            refresh();
+        });
+    };
 
     $scope.edit = function(contact) {
         $scope.contact = { name: contact.name, email: contact.email, number: contact.number, _id: contact._id };
-    }
+    };
 
     $scope.update = function() {
         console.log($scope.contact._id)
         $http.put('/contactlist/' + $scope.contact._id, $scope.contact).success(function(response) {
-            refresh()
-        })
-    }
+            refresh();
+        });
+    };
 
     $scope.clear = function() {
-        $scope.contact = ""
-    }
+        $scope.contact = "";
+    };
 
-}])
+}]);
--- server.js
+++ server.js
@@ -1,59 +1,59 @@
-var express = require('express')
-var app = express()
-var mongojs = require('mongojs')
-var db = mongojs('contactlist', ['contactlist'])
-var bodyParser = require('body-parser')
+var express = require('express');
+var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('contactlist', ['contactlist']);
+var bodyParser = require('body-parser');
 var port = Number(process.env.PORT || 8000)
 
 
 
-app.use(express.static(__dirname + "/public"))
-app.use(bodyParser.json())
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
 
 
 app.get('/contactlist', function(req, res) {
-    console.log("I received a GET request")
+    console.log("I received a GET request");
 
     db.contactlist.find(function(err, docs) {
-        console.log(docs)
-        res.json(docs)
-    })
-})   
+        console.log(docs);
+        res.json(docs);
+    });
+});  
      
 app.post('/contactlist', function(req, res) {
-    req.body._id = 0
+    req.body._id = 0;
     db.contactlist.insert(req.body, function(err, doc) {
-        res.json(doc)
-    })
-})﻿
+        res.json(doc);
+    });
+})﻿;
 
 app.delete('/contactlist/:id', function(req, res) {
-    var id = req.params.id
-    console.log(id)
+    var id = req.params.id;
+    console.log(id);
     db.contactlist.remove({ _id: mongojs.ObjectId(id) }, function(err, doc) {
-        res.json(doc)
-    })
-})
+        res.json(doc);
+    });
+});
 
 app.get('/contactlist/:id', function(req, res) {
-    var id = req.params.id
-    console.log(id)
+    var id = req.params.id;
+    console.log(id);
     db.contactlist.fineOne({ _id: mongojs.ObjectId(id) }, function(err, doc) {
-        res.json(doc)
-    })
-})
+        res.json(doc);
+    });
+});
 
 app.put('/contactlist/:id', function(req, res) {
-    var id = req.params.id
-    console.log(req.body.name)
+    var id = req.params.id;
+    console.log(req.body.name);
     db.contactlist.findAndModify({
         query: { _id: mongojs.ObjectId(id) },
         update: { $set: { name: req.body.name, email: req.body.email, number: req.body.number } },
         new: true
     }, function(err, doc) {
-        res.json(doc)
-    })
-})
+        res.json(doc);
+    });
+});
 
 app.listen(port)
     console.log("App is running on port 8000")
